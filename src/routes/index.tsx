7a41e1752a383fa59d9122fbae1199043ab5e720
binaryvisionai-beep <binaryvisionai@gmail.com>
2026-05-16 18:10:13 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -39,7 +39,7 @@ function Index() {
         <Industries />
         <CaseStudies />
         <Stats />
-        <Pricing />
+        {/* <Pricing /> */}
         <Testimonials />
         <Blog />
         <CtaStrip />
